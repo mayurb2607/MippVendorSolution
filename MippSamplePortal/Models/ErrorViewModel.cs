ae1ada5cce32eb49211531db13ebb65d480e0d2b
Mayur Bhavsar <Mayur.b@anarsolutions.com>
2023-06-20 22:54:42 +0530

    Add project files.

---
+++ MippSamplePortal/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MippSamplePortal.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
